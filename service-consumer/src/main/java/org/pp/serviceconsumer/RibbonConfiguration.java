5fc9760038ef03fb1ca967a7f53417949834a134
GitJavaProgramming <455547940@qq.com>
2020-02-13 10:54:48 +0800

    springcloud eureka zuul feign ribbon hystrix小例子 eureka注册中心 zuul网关 eureka客户端 feign rest Api ribbon请求负载均衡 hystrix熔断降级处理

---
+++ service-consumer/src/main/java/org/pp/serviceconsumer/RibbonConfiguration.java
@@ -0,0 +1,15 @@
+package org.pp.serviceconsumer;
+
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.IRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RibbonConfiguration {
+
+    @Bean
+    public IRule ribbonRule() {
+        return new BestAvailableRule();
+    }
+}

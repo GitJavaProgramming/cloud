62e42b8bd1eec60b4aef284b3c987fa2b29b8bdc
GitJavaProgramming <455547940@qq.com>
2020-02-21 14:41:38 +0800

    去除同源策略配置，可正常访问

--- service-consumer/src/main/java/org/pp/consumer/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/org/pp/consumer/ServiceConsumerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@Deprecated
 @EnableCircuitBreaker/*断路器*/
 @EnableFeignClients/*扫描所有@FeignClient注解*/
 @EnableDiscoveryClient
--- service-gateway/src/main/java/org/pp/servicegateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/org/pp/servicegateway/ServiceGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
+@Deprecated
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication

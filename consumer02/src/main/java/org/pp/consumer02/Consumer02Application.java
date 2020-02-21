092c6b9b11d4cb0c92182e4ce02a93bfccd0c6f0
GitJavaProgramming <455547940@qq.com>
2020-02-21 11:23:14 +0800

    使用声明式api 使用REST风格

--- consumer02/src/main/java/org/pp/consumer02/Consumer02Application.java
+++ consumer02/src/main/java/org/pp/consumer02/Consumer02Application.java
@@ -3,7 +3,9 @@ package org.pp.consumer02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Consumer02Application {
--- consumer02/src/main/java/org/pp/consumer02/FeignClientFallback.java
+++ consumer02/src/main/java/org/pp/consumer02/FeignClientFallback.java
@@ -2,10 +2,9 @@ package org.pp.consumer02;
 
 import org.springframework.stereotype.Component;
 
-@Component
-public class FeignClientFallback implements ServiceProviderFeignClient {
+//@Component
+public class FeignClientFallback/* implements ServiceProviderFeignClient */{
 
-    @Override
     public String hello() {
         return "你好，断路了。";
     }
